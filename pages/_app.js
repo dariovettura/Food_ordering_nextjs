860b191ac572df561437236c46d5c6fd4ffe846e
dariovettura <d.vettura.wd@gmail.com>
2021-10-08 11:30:14 +0200

    gif

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,37 @@ function MyApp({ Component, pageProps }) {
   return(
     <React.Fragment>
       <Head>
-        <title>My page</title>
+      <meta name='application-name' content='Donuts' />
+<meta name='apple-mobile-web-app-capable' content='yes' />
+<meta name='apple-mobile-web-app-status-bar-style' content='default' />
+<meta name='apple-mobile-web-app-title' content='Donuts' />
+<meta name='description' content='Le migliori Donuts del mondo' />
+<meta name='format-detection' content='telephone=no' />
+<meta name='mobile-web-app-capable' content='yes' />
+<meta name='msapplication-config' content='/doughnut_color.png' />
+<meta name='msapplication-TileColor' content='#2B5797' />
+<meta name='msapplication-tap-highlight' content='no' />
+<meta name='theme-color' content='#000000' />
+
+<link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
+<link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
+<link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
+<link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
+
+<link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+<link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+<link rel='manifest' href='/manifest.json' />
+<link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
+<link rel='shortcut icon' href='/favicon.ico' />
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+     
+
+<meta property='og:type' content='website' />
+<meta property='og:title' content='Donuts' />
+<meta property='og:description' content='Le migliori Donuts del mondo' />
+<meta property='og:site_name' content='Donuts' />
+<meta property='og:url' content='https://food-ordering-nu.vercel.app/' />
+<meta property='og:image' content='https://food-ordering-nu.vercel.app/doughnut_color.png' />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
      
